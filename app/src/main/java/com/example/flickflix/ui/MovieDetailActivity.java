e9533230c6c50b172c5af0e20cdf116a75edbabf
StanTeurlings <s_lings@hotmail.com>
2024-04-03 13:03:46 +0200

    sharing

--- app/src/main/java/com/example/flickflix/ui/MovieDetailActivity.java
+++ app/src/main/java/com/example/flickflix/ui/MovieDetailActivity.java
@@ -3,9 +3,12 @@ package com.example.flickflix.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,26 +17,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.flickflix.R;
-import com.example.flickflix.data.response.VideoResponse;
 import com.example.flickflix.model.Genre;
 import com.example.flickflix.model.Movie;
 import com.example.flickflix.model.Video;
-import com.example.flickflix.ui.adapter.MovieListAdapter;
 import com.example.flickflix.viewmodel.GenreViewModel;
 import com.example.flickflix.viewmodel.MovieViewModel;
 import com.example.flickflix.viewmodel.VideoViewModel;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.FullscreenListener;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import kotlin.Unit;
-import kotlin.jvm.functions.Function0;
 
 public class MovieDetailActivity extends AppCompatActivity {
     private final String LOG_TAG = this.getClass().getSimpleName();
@@ -45,6 +38,7 @@ public class MovieDetailActivity extends AppCompatActivity {
     VideoViewModel videoViewModel;
     private List<Genre> genres = new ArrayList<>();
     private List<Video> videos = new ArrayList<>();
+    private Movie mShareMovie;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -70,6 +64,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         assert mMovie != null;
 
         movieViewModel.getMovie(mMovie.getId()).observe(this, movie -> {
+            mShareMovie = movie;
 
             ActionBar toolbar = getSupportActionBar();
             if (null != toolbar) {
@@ -79,32 +74,27 @@ public class MovieDetailActivity extends AppCompatActivity {
             Picasso.get().load(movie.getFullBackdropPath()).into(imgMovieBanner);
             tvMovieDetails.setText(getMovieDetails(movie));
             tvMovieDescription.setText(movie.getOverview());
-
-            YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
-            getLifecycle().addObserver(youTubePlayerView);
-
-            youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-                @Override
-                public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                    videoViewModel.getVideos(mMovie.getId()).observe(MovieDetailActivity.this, videoResponse -> {
-                        videos = videoResponse.getVideos(mMovie.getId());
-                        if (!videos.isEmpty()) {
-                            String videoId = null;
-                            for (Video video : videos) {
-                                if (video.getOfficial() == true && video.getType().equals("Trailer")) {
-                                    videoId = video.getKey();
-                                    break;
-                                }
-                            }
-                            youTubePlayer.loadVideo(videoId, 0);
-                        }
-                    });
-                }
-            });
         });
     }
 
-    private StringBuilder getMovieDetails(Movie mMovie) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_movie_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_extra_button) {
+            // Handle the click event for the share button
+            shareMovieDetails();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private String getMovieDetails(Movie mMovie) {
         StringBuilder details = new StringBuilder();
 
         details.append(mMovie.getRuntime());
@@ -116,7 +106,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         details.append(mMovie.getFormattedVoteAverage());
         details.append("/10");
 
-        return details;
+        return details.toString();
     }
 
     private String getGenresForMovie(Movie movie) {
@@ -124,7 +114,6 @@ public class MovieDetailActivity extends AppCompatActivity {
 
         if (movie.getGenres() != null && !movie.getGenres().isEmpty()) {
             int size = movie.getGenres().size();
-            Log.i(LOG_TAG, String.valueOf(size));
             for (int i = 0; i < size; i++) {
                 if (genreStringBuilder.length() > 0) {
                     // Add an "," or "and" between the genres
@@ -138,5 +127,19 @@ public class MovieDetailActivity extends AppCompatActivity {
             }
         }
         return genreStringBuilder.toString();
+    }
+
+    private void shareMovieDetails() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, getMovieDetails(mShareMovie));
+        sendIntent.setType("text/plain");
+        sendIntent.setPackage("com.whatsapp");
+
+        try {
+            startActivity(sendIntent);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "WhatsApp is not installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }

619b97149f40119d5396c66a0b4178dce51b9563
Jacob Timmerman <75219092+Jacobtims@users.noreply.github.com>
2024-04-05 19:30:20 +0200

    Revert "Revert "Movie list overview""

--- app/src/main/java/com/example/flickflix/data/SharedPreferencesManager.java
+++ app/src/main/java/com/example/flickflix/data/SharedPreferencesManager.java
@@ -7,6 +7,8 @@ import android.content.SharedPreferences;
 
 public class SharedPreferencesManager {
     private static final String NAME = "SessionSharedPreferences";
+    private static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+    private static final String ACCOUNT_ID = "ACCOUNT_ID";
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
     Context context;
@@ -17,17 +19,27 @@ public class SharedPreferencesManager {
         editor = sharedPreferences.edit();
     }
 
-    public void saveSession(String sessionId) {
-        editor.putString("SESSION_ID", sessionId);
+    public void saveAccessToken(String accessToken) {
+        editor.putString(ACCESS_TOKEN, accessToken);
         editor.commit();
     }
 
-    public String getSessionId() {
-        return sharedPreferences.getString("SESSION_ID", null);
+    public String getAccessToken() {
+        return sharedPreferences.getString(ACCESS_TOKEN, null);
     }
 
-    public void deleteSession() {
-        editor.remove("SESSION_ID");
+    public void saveAccountId(String accountId) {
+        editor.putString(ACCOUNT_ID, accountId);
+        editor.commit();
+    }
+
+    public String getAccountId() {
+        return sharedPreferences.getString(ACCOUNT_ID, null);
+    }
+
+    public void deleteCredentials() {
+        editor.remove(ACCESS_TOKEN);
+        editor.remove(ACCOUNT_ID);
         editor.commit();
     }
 }
---
+++ app/src/main/java/com/example/flickflix/model/MovieList.java
@@ -0,0 +1,40 @@
+package com.example.flickflix.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MovieList {
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("account_object_id")
+    private String accountObjectId;
+
+    // Getter and setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and setter for description
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAccountObjectId() {
+        return accountObjectId;
+    }
+
+    public void setAccountObjectId(String accountObjectId) {
+        this.accountObjectId = accountObjectId;
+    }
+}
